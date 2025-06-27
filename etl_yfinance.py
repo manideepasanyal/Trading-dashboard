141cf2f8e26b01c649f47f30ece952f0b596b012
manideepasanyal <115183771+manideepasanyal@users.noreply.github.com>
2025-06-27 20:24:36 +0200

    Update etl_yfinance.py

--- etl_yfinance.py
+++ etl_yfinance.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 import streamlit as st
 DATABASE_URL = st.secrets["DATABASE_URL"]
 engine = create_engine(DATABASE_URL)
-symbols = ['AAPL', 'MSFT', 'TSLA', 'NVDA', '    qnccf]
+symbols = ['AAPL', 'MSFT', 'TSLA', 'NVDA', 'QNCCF']
 
 all_data = []
 
